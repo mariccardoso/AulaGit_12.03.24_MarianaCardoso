cab8c1e0d844fba430e82b7b659d98fa8f35311a
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-03-12 15:09:00 -0300

    Valores dos números alterados

---
+++ DesafioAvaliativo.js
@@ -0,0 +1,32 @@
+
+// Solicitar os dois números 
+let a = 90;
+let b = 50;
+
+// Determinar o inicio e o fim
+let inicio = a;
+let fim = b;
+
+if (inicio < fim) {
+    inicio = a;
+    fim = b;
+
+} else {
+    inicio = b;
+    fim = a;
+}
+
+// Fazer a operação (total de números ímpares, soma e sequencia)
+let total = 0;
+let soma = 0;
+for (let i = inicio; i <= fim; i++) {
+    // Se o resto da divisão (%) por 2 for diferente de 0...
+    if (i % 2 != 0) {
+        total++;
+        soma += i;
+    }
+}
+// Exibir resultados
+console.log("Total de números ímpares: " + total);
+console.log("Soma: " + soma);
+
