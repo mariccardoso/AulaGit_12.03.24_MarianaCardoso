ac5e40faa395cb2ac47d7aa2926b0dcc89e49577
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-03-12 15:43:46 -0300

    Código alterado de acordo com as mudanças das variáveis

--- DesafioAvaliativo.js
+++ DesafioAvaliativo.js
@@ -4,16 +4,16 @@ let y = 90;
 let z = 50;
 
 // Determinar o inicio e o fim: inicio sempre o menor número e o fim o maior
-let inicio = a;
-let fim = b;
+let inicio = y;
+let fim = z;
 
 if (inicio < fim) {
-    inicio = a;
-    fim = b;
+    inicio = y;
+    fim = z;
 
 } else {
-    inicio = b;
-    fim = a;
+    inicio = z;
+    fim = y;
 }
 
 // Fazer a operação (total de números ímpares, soma e sequencia)
